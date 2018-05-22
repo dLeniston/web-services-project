c485c53c142a9a90db24ab89a5d87cb326c26303
Darren Leniston <dleniston@yahoo.com>
2018-05-22 08:21:27 +0000

    Refactored Recipe Routes to use Async Functions

--- app/helpers/recipe.js
+++ app/helpers/recipe.js
@@ -1,8 +1,8 @@
-var express     = require("express");
-var router      = express.Router();
-var middleware  = require("../middleware");
-var Recipe  = require("../models/recipe");
-var Comment     = require("../models/comment");
+const express     = require("express");
+const router      = express.Router();
+const middleware  = require("../middleware");
+const Recipe  = require("../models/recipe");
+const Comment     = require("../models/comment");
 
 //Get new recipe form
 router.get("/new", middleware.isLoggedIn, (req, res) =>{
@@ -10,73 +10,66 @@ router.get("/new", middleware.isLoggedIn, (req, res) =>{
 });
 
 //Create new recipe
-router.post('/', middleware.isLoggedIn, (req, res) => {
-  //create recipe
-  var name = req.body.name;
-    var img = req.body.img;
-    var ingredients = req.body.ingredients;
-    var instructions = req.body.instructions;
-    var created = req.body.created;
-    var author = {
-        id: req.user._id,
-        username: req.user.username
-    };
-    var newRecipe = {name: name, img: img, ingredients: ingredients, instructions: instructions, created: created, author: author};
-    //create the new recipe object and add to db
-    Recipe.create(newRecipe)
-    .then(function(newRecipe){
-      //redirect to index
+router.post('/', middleware.isLoggedIn, async function(req, res, next){
+    try{
+      let recipe = await Recipe.create({
+        name: req.body.name,
+        img: req.body.img,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        created: req.body.created,
+        author: {
+          id: req.user._id,
+          username: req.user.username
+        }
+      });
+      await recipe.save();
       res.redirect('/');
-    }).catch(function(err){
-      res.send(err);
-    });
+    }catch(err){
+      return next(err);
+    }
 });
 
 //Show a the recipe page with selected recipe information
-router.get("/:id", (req, res) =>{
-  //find one recipe with correct id
-  Recipe.findById(req.params.id).populate("comments").exec(function(err, foundRecipe){
-    if(err){
-      res.setStatus(500, err);
-    }else{
-    //show the recipe that has been selecteds data on the show recipe view
-    res.render("recipe", {recipe: foundRecipe});
-    }
-  });
+router.get('/:id', async function(req, res, next){
+  try{
+    let foundRecipe = await Recipe.findById(req.params.id).populate("comments");
+    res.render('recipe', {recipe: foundRecipe});
+  }catch(err){
+    return next(err);
+  }
 });
 
 //Get recipe edit form
-router.get("/:id/edit", middleware.checkRecipeOwnership, (req, res) => {
-  Recipe.findById(req.params.id).then(function(foundRecipe){
-    //render the edit form with the form filled in with the recipe to be updated current data
-    res.render("editRecipe", {recipe: foundRecipe});
-  }).catch(function(err){
-    res.send(err);
-  });
+router.get('/:id/edit', middleware.checkRecipeOwnership, async function(req, res, next){
+  try{
+    let foundRecipe = await Recipe.findById(req.params.id);
+    await res.render('editRecipe', {recipe: foundRecipe});
+  }catch(err){
+    return next(err);
+  }
 });
 
 //Update a recipe
-
-router.put("/:id", middleware.checkRecipeOwnership, (req, res) =>{
-  //update the recipe
-  Recipe.findByIdAndUpdate(req.params.id, req.body.recipe).then(function(updatedRecipe){
-    //redirect to the updated recipes show page
-    res.redirect("/recipes/" + req.params.id);
-  }).catch(function(err){
-    res.sendStatus(500, err);
-  });
+router.put('/:id', middleware.checkRecipeOwnership, async function(req, res, next){
+  try{
+      let updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body.recipe);
+      await updatedRecipe.save();
+      res.redirect('/recipes/' + req.params.id);
+  }catch(err){
+    return next(err);
+  }
 });
 
 //Delete a selected recipe
-
-router.delete("/:id", middleware.checkRecipeOwnership, (req, res) => {
-  //delete recipe
-  Recipe.findByIdAndRemove(req.params.id).then(function(){
-    //redirect to index page
-    res.redirect('/');
-  }).catch(function(err){
-    res.sendStatus(500, err);
-  });
+router.delete('/:id', middleware.checkRecipeOwnership, async function(req, res, next){
+  try{
+      let foundRecipe = await Recipe.findById(req.params.id);
+      await foundRecipe.remove();
+      res.redirect('/');
+  }catch(err){
+    return next(err);
+  }
 });
 
 module.exports = router;
